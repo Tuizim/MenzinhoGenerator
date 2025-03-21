83edb037c3c69eb4da7ac625d8ce3ace206de9fc
Artur Trombim <114882647+Tuizim@users.noreply.github.com>
2025-03-21 17:31:04 -0300

    [FIX] Bug fixed

--- Artur.java
+++ Artur.java
@@ -1,21 +1,24 @@
-public class Artur {
-    static final String name = "ARTUR TROMBIM DE SOUSA";
-    static final String currentPosition = "trainee and studant";
-    static final List<String> SomeSkills= ["Music","Teaching","Organization"];
-    static final List<String> 
-    languages = [ "Java", "C#", "Python","SQL" ];
-    }
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-    public class Mateus extends Artur {
-        static final String name = "MATEUS ROSOLEM BARONI";
+class Artur {
+    static final String name = "ARTUR TROMBIM DE SOUSA";
+    static final String currentPosition = "Software Development Intern";
+    static final List<String> SomeSkills = Arrays.asList("Music", "Teaching", "Organization");
+    static final List<String> languages = Arrays.asList("Java", "C#", "Python", "SQL");
+}
+
+public class Mateus extends Artur {
+    static final String name = "MATEUS ROSOLEM BARONI";
     
-        static final List<String> SomeSkills = new ArrayList<String>() {{
-            add("Coding");
-            add("Goiabada");
-        }};
-        
-        static final List<String> languages = new ArrayList<String>() {{
-            add("Airpods");
-            add("SmartFit");
-        }
-};
+    static final List<String> SomeSkills = new ArrayList<String>() {{
+        add("Coding");
+        add("Goiabada");
+    }};
+    
+    static final List<String> languages = new ArrayList<String>() {{
+        add("Airpods");
+        add("SmartFit");
+    }};
+}
